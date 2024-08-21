76edcbcad5beea15e837d234c2fdd32f420b8deb
Thiaguiner <tcarneiro990@gmail.com>
2024-08-21 11:16:28 -0300

    mudando a porta de acesso

--- src/main/java/com/thiaguiner/course/resources/UserResource.java
+++ src/main/java/com/thiaguiner/course/resources/UserResource.java
@@ -1,21 +1,21 @@
-package com.thiaguiner.course.resources;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.thiaguiner.course.entities.User;
-
-@RestController
-@RequestMapping(value = "/users")
-public class UserResource {
-	
-//controlodor que responde no caminho "/users"
-	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1, "maria", "maria@gmail.com", "999999", "12345");
-		return ResponseEntity.ok().body(u);
-	}
-	
-}
+//package com.thiaguiner.course.resources;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.thiaguiner.course.entities.User;
+//
+//@RestController
+//@RequestMapping(value = "/users")
+//public class UserResource {
+//	
+////controlodor que responde no caminho "/users"
+//	@GetMapping
+//public ResponseEntity<User> findAll(){
+//		User u = new User(1, "maria", "maria@gmail.com", "999999", "12345");
+//		return ResponseEntity.ok().body(u);
+//	}
+//	
+//}
